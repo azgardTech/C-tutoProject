3c31612e35f97ce99c1c880d873c27a43d66d598
azgardTech <khreibi.mohamed@gmail.com>
2016-09-25 21:13:11 +0200

    add QG controle Stores class

---
+++ MonMagasin/EnsMagasins.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonMagasin
+{
+    class EnsMagasins
+    {
+        public HashSet<Magasin> MyStores { get; set; }
+
+        public EnsMagasins()
+        {
+            MyStores = new HashSet<Magasin>();
+        }
+
+        public void AddStore(Magasin m)
+        {
+            Boolean ok = false;
+
+            foreach(Magasin mag in this.MyStores.TakeWhile(i=>!ok))
+            {
+                ok = Magasin.ToCompareStoreId(mag, m);
+            }
+
+            if(!ok)
+            {
+                this.MyStores.Add(m);
+                Console.WriteLine("Add product complete with success..." ); }
+            else
+            { Console.WriteLine("Store already exist..."); }
+        }
+
+//********************to complete
+        public void DeleteStore(Magasin m)
+        { }
+
+        public void SearchStore(Magasin m)
+        { }
+
+    }
+}
--- MonMagasin/Magasin.cs
+++ MonMagasin/Magasin.cs
@@ -25,6 +25,15 @@ namespace MonMagasin
             h = new HashSet<Produit>();      
         }
 
+        public static Magasin MakeMagasin()
+        {
+            Magasin m = new Magasin(Int32.Parse(Program.StringChecker(@"^\d+$", "id: "))
+                ,Program.StringChecker(@"\w+","adresse: ")
+                ,Int32.Parse(Program.StringChecker(@"^\d+$","Capacité: ")));
+
+
+            return m;
+        }
 
 
         public void MagasinShow()
@@ -73,6 +82,30 @@ namespace MonMagasin
             foreach(Produit prd in this.h.TakeWhile(i=>!ok))
             { ok = Produit.ToCompare(prd, p); }
             return ok;
+        }
+
+
+        public static void ToCompareNbrProducts(Magasin m1, Magasin m2)
+        {
+            if (m1.h.Count() < m2.h.Count())
+            { Console.WriteLine("Store with id "+m2.id+" has more products"); }
+            else
+            {
+                if (m1.h.Count() == m2.h.Count())
+                { Console.WriteLine("Store 1 and 2 have the same number of products"); }
+                else
+                {
+                    if (m1.h.Count() > m2.h.Count())
+                    { Console.WriteLine("Store with id " + m1.id + " has more products"); }
+                }
+            }
+
+  
+        }
+
+        public static Boolean ToCompareStoreId(Magasin m1, Magasin m2)
+        {
+            return (m1.id == m2.id && m1.adresse == m2.adresse && m1.capacite == m2.capacite);
         }
 
 
